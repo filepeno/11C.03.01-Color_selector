911a0305c1656332a6ed8bbcb230fd4b28632b17
Filip <fili4252@stud.kea.dk>
2021-09-07 08:31:51 +0200

    added style

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+const colorPicker = document.querySelector("input");
+
+function start() {
+  console.log("start()");
+  trackColorPicker();
+}
+
+function trackColorPicker() {
+  console.log("trackColorPicker()");
+  console.log(colorPicker.value);
+  colorPicker.addEventListener("input", updateColor, false);
+}
+
+function updateColor() {
+  console.log(colorPicker.value);
+}
+
+function calculateRGB() {}
+
+function calculateHSL() {}
